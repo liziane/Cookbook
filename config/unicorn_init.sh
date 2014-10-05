8f8fa8c9f82804887780022df3d9af154020cc03
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2014-10-05 15:41:19 -0300

    uses unicorn bin from rbenv shims

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -5,7 +5,7 @@ TIMEOUT=${TIMEOUT-60}
 APP_NAME=cookbook
 APP_ROOT=/home/pico/apps/cookbook/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
-CMD="cd $APP_ROOT && bin/unicorn -c $APP_ROOT/config/unicorn.rb -E production -D"
+CMD="cd $APP_ROOT && unicorn -c $APP_ROOT/config/unicorn.rb -E production -D"
 USER=pico
 action="$1"
 set -u
