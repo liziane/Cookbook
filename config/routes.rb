2edb337121ab9fc87b5b309851f8f4f9d3f652b9
Lizi <liziane.brandini@gmail.com>
2016-01-07 22:00:51 -0800

    samerda ta uma merda

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
+  get 'messages' => 'messages#index'
+
+
 end
